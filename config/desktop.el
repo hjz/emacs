4a6fc9dbbaa1db4962bba9c79f06bf9f99c7e70e
Justin Zhu <jz@twitter.com>
2011-07-17 17:25:22 -0700

    save file cache to disk

--- config/desktop.el
+++ config/desktop.el
@@ -41,18 +41,18 @@
 (add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
 (add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
 
-(defun emacs-process-p (pid)
-  "If pid is the process ID of an emacs process, return t, else nil.
-Also returns nil if pid is nil."
-  (when pid
-    (let ((attributes (process-attributes pid)) (cmd))
-      (dolist (attr attributes)
-        (if (string= "comm" (car attr))
-            (setq cmd (cdr attr))))
-      (if (and cmd (or (string= "emacs" cmd) (string= "Emacs.app" cmd))) t))))
+;; (defun emacs-process-p (pid)
+;;   "If pid is the process ID of an emacs process, return t, else nil.
+;; Also returns nil if pid is nil."
+;;   (when pid
+;;     (let ((attributes (process-attributes pid)) (cmd))
+;;       (dolist (attr attributes)
+;;         (if (string= "comm" (car attr))
+;;             (setq cmd (cdr attr))))
+;;       (if (and cmd (or (string= "emacs" cmd) (string= "Emacs.app" cmd))) t))))
 
-(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
-  "Don't allow dead emacsen to own the desktop file."
-  (when (not (emacs-process-p ad-return-value))
-    (setq ad-return-value nil)))
+;; (defadvice desktop-owner (after pry-from-cold-dead-hands activate)
+;;   "Don't allow dead emacsen to own the desktop file."
+;;   (when (not (emacs-process-p ad-return-value))
+;;     (setq ad-return-value nil)))
 
--- config/filecache.el
+++ config/filecache.el
@@ -31,31 +31,55 @@ directory, select directory. Lastly the file is opened."
 (add-to-list 'file-cache-filter-regexps "project/boot")
 (add-to-list 'file-cache-filter-regexps "target")
 
-(add-to-list 'file-cache-filter-regexps "\\.sw\w$")
-(add-to-list 'file-cache-filter-regexps "/#.*")
+;(add-to-list 'file-cache-filter-regexps "\\.sw\w$")
+(add-to-list 'file-cache-filter-regexps "/\\..*")
+;;(add-to-list 'file-cache-filter-regexps "/\\.#.*")
 (add-to-list 'file-cache-filter-regexps "\\.class$")
 (add-to-list 'file-cache-filter-regexps "\\.gz$")
 (add-to-list 'file-cache-filter-regexps "\\.jpg$")
 (add-to-list 'file-cache-filter-regexps "\\.gif$")
 (add-to-list 'file-cache-filter-regexps "\\.png$")
 (add-to-list 'file-cache-filter-regexps "\\.jar$")
+(add-to-list 'file-cache-filter-regexps "\\.pyc$")
 (add-to-list 'file-cache-filter-regexps "\\.svn-base$")
 (add-to-list 'file-cache-filter-regexps "\\.dump$")
-(add-to-list 'file-cache-filter-regexps "/[.]\w+$")
-(add-to-list 'file-cache-filter-regexps "/[.]git")
-(add-to-list 'file-cache-filter-regexps "/[.]svn")
+(add-to-list 'file-cache-filter-regexps "/\\.\w+$")
+(add-to-list 'file-cache-filter-regexps "/\\.git")
+(add-to-list 'file-cache-filter-regexps "/\\.svn")
+
+(defvar file-cache-file "~/.file_cache")
 
 (defun file-cache-refresh ()
+  (interactive)
   (message "Loading file cache...")
   (file-cache-clear-cache)
   (file-cache-add-directory-using-find "~/ps/birdcage")
-;  (file-cache-add-directory-using-find "~/ps/science")
   (file-cache-add-directory-using-find "~/ps/macaw")
   (file-cache-add-directory-using-find "~/ps/querulous")
-;  (file-cache-add-directory-using-find "~/ps/twitter")
+  (file-cache-add-directory-using-find "~/ps/twitter")
   (file-cache-add-directory-using-find dotfiles-dir)
-  ;	    (file-cache-add-directory "~/")
-  ;	    (file-cache-add-file-list (list "~/foo/bar" "~/baz/bar"))
-  )
+  ;; (file-cache-add-directory-using-find "~/ps/science")
+  ;; (file-cache-add-directory "~/")
+  ;; (file-cache-add-file-list (list "~/foo/bar" "~/baz/bar"))
+  (file-cache-save-cache-to-file)
+  (message "File cache loaded and saved to %s" file-cache-file))
 
-(file-cache-refresh)
+(defun file-cache-save-cache-to-file ()
+  "Save contents of `file-cache-alist' to ~/.file_cache.
+For later retrieval using `file-cache-read-cache-from-file'"
+  (interactive)
+  (with-temp-file (expand-file-name file-cache-file)
+    (prin1 file-cache-alist (current-buffer))))
+
+(defun file-cache-read-cache-from-file ()
+  "Clear `file-cache-alist' and read cache from FILE.
+  The file cache can be saved to a file using
+  `file-cache-save-cache-to-file'."
+  (interactive)
+  (file-cache-clear-cache)
+  (save-excursion
+    (set-buffer (find-file-noselect file-cache-file))
+    (beginning-of-buffer)
+    (setq file-cache-alist (read (current-buffer)))))
+
+(file-cache-read-cache-from-file)
